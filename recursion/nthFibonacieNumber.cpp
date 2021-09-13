7e1a9642507621fa869b8d1a3d06c51010dd468a
Shailja Sengar <65380554+shailja-singh@users.noreply.github.com>
2021-09-14 00:49:54 +0530

    Add files via upload

---
+++ recursion/nthFibonacieNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+    if(n < 2){
+        return n;
+    }
+
+    return fib(n-1) + fib(n-2);
+}
+int main(){
+    int n =4;
+    
+
+    cout << fib(n);
+
+    return 0;
+}
