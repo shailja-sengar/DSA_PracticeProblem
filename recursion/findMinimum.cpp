4a11c320ce50eb69b9d1674ea6b5effebcbfc651
Shailja Sengar <65380554+shailja-singh@users.noreply.github.com>
2021-09-16 00:18:12 +0530

    Update findMinimum.cpp

--- recursion/findMinimum.cpp
+++ recursion/findMinimum.cpp
@@ -2,19 +2,19 @@
 #include<vector>
 using namespace std;
 
-int maximum(vector<int>&arr, int n){
+int minimum(vector<int>&arr, int n){
     if(n == 1){
         return arr[0] ;
     }
   
-   return min(arr[n-1], maximum(arr, n-1));
+   return min(arr[n-1], minimum(arr, n-1));
     
 }
 int main(){
     vector<int> arr = {2,1234,4,5,1};
     int n = arr.size();
 
-    cout<<maximum(arr, n);
+    cout<<minimum(arr, n);
 
     return 0;
 }
